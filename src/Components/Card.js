af0a6b4145737eace46a5723be723a5f84922058
Seung Wook Lee <seungwooklee94@gmail.com>
2019-10-15 16:58:24 +0900

    중도

---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+// const Card = styled.div`
+//   padding: ${props => String(props.padding) + "px"};
+// `;
+
+// const CardCaption = styled.span``;
+
+// export default ({ svgElement, size = "100", color = "white", text }) => {
+//   return (
+//   <Card padding={Number(size) / 5}>
+
+//     <svgElement></svgElement>
+//   </Card>);
+// };
