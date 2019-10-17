0286d856d63899a84e0687710c51784067451189
Seung Wook Lee <seungwooklee94@gmail.com>
2019-10-17 13:46:25 +0900

    Updates

---
+++ precache-manifest.b6b3b4bf3f9ea16cb05af283404cd9cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8823908a1a30842cc0c313618b2cbfb",
+    "url": "/slee333.github.io/index.html"
+  },
+  {
+    "revision": "b7e470658611ad7499b1",
+    "url": "/slee333.github.io/static/js/2.69801e02.chunk.js"
+  },
+  {
+    "revision": "86e4ec6d6c63235b5abd",
+    "url": "/slee333.github.io/static/js/main.1c1bbf37.chunk.js"
+  },
+  {
+    "revision": "ee8562c6704a799e368b",
+    "url": "/slee333.github.io/static/js/runtime-main.55e1ecdc.js"
+  },
+  {
+    "revision": "0e8662c97df2306f332fcc3fecc32b70",
+    "url": "/slee333.github.io/static/media/PaperBG.0e8662c9.png"
+  },
+  {
+    "revision": "af1572bdaa595a90a2ba4a5298523a24",
+    "url": "/slee333.github.io/static/media/나눔손글씨딸에게엄마가.af1572bd.ttf"
+  },
+  {
+    "revision": "7aa608b005b47f328af68d0eab74cdd0",
+    "url": "/slee333.github.io/static/media/나눔손글씨성실체.7aa608b0.ttf"
+  },
+  {
+    "revision": "27d7c08165131496c0d97e7c69b7adba",
+    "url": "/slee333.github.io/static/media/미생체.27d7c081.ttf"
+  },
+  {
+    "revision": "797e0ca50dc69a890b35740e38691a51",
+    "url": "/slee333.github.io/static/media/전소민체.797e0ca5.ttf"
+  },
+  {
+    "revision": "81a4ca4d195ff0d8ce1c81695555dbc0",
+    "url": "/slee333.github.io/static/media/할아버지의나눔.81a4ca4d.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.187720c76d794f75c8af4a62194fbfe6.js"
+  "/slee333.github.io/precache-manifest.b6b3b4bf3f9ea16cb05af283404cd9cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slee333.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
